003702bfa3020339fdcdd206e798f5b997f1212e
theZMC <zach@zmc.dev>
2023-03-05 12:04:07 -0600

    reduce: cleanup signature

--- interface.go
+++ interface.go
@@ -5,17 +5,15 @@ type Of[T any] interface {
 	// Next returns the next value in the iterator, consuming it in the process, as well as a boolean indicating whether
 	// there was a value to return. If there was no value to return, the returned value will be the zero value for the type.
 	Next() (T, bool)
-	// ForEach iterates over the iterator, calling the given function for each value and consuming the iterator. Calls to
-	// ForEach are thread-safe as long as the provided function is thread-safe, but it's not recommended to call ForEach
-	// from multiple goroutines, especially if the provided function is different for each goroutine.
+	// ForEach iterates over the iterator, calling the given function for each value and consuming the iterator.
 	ForEach(fn func(T))
-	// Map returns a new iterator that applies the given function to each value in the iterator upon collection. The function
+	// Map returns a new iterator that applies the given function to each value in the iterator. The function
 	// is lazily evaluated, so it is not applied until the iterator is collected.
 	Map(fn func(T) T) Of[T]
 	// Filter returns a new iterator that keeps only the values in the iterator that return true when passed to the given
-	// function upon collection. The function is lazily evaluated, so it is not applied until the iterator is collected.
+	// function. The function is lazily evaluated, so it is not applied until the iterator is collected.
 	Filter(fn func(T) bool) Of[T]
-	// Unique returns a new iterator that filters out duplicate values in the iterator upon collection. The function is
+	// Unique returns a new iterator that filters out duplicate values in the iterator. The function is
 	// lazily evaluated, so it is not applied until the iterator is collected. This is a convenience method that is equivalent
 	// to calling Filter with a function that keeps track of the values it has seen. If the iterator contains pointers, the
 	// DerefPointers option can be used to dereference the pointers before evaluating uniqueness.
@@ -43,7 +41,7 @@ type Of[T any] interface {
 	// Reduce applies the given function to each value in the iterator, passing the result of the previous function call as the
 	// first argument and the next value as the second argument until there are no more values. The initial value is passed to
 	// the anonymous function as the first argument on the first iteration.
-	Reduce(fn func(accumulator T, next T) T, initial T) T
+	Reduce(fn func(accumulator, next T) T, initial T) T
 	// Reset resets the iterator to the beginning of the source slice. This is useful if you want to iterate over the same
 	// slice multiple times. Note that this does not reset the chained map and filter operations. If you want to reset those,
 	// you should create a new iterator using the From function.
